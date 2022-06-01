966d50fa72baf064b28cb46f35155227741c40bc
kareemikassab7 <kareemikassab@aucegypt.edu>
2022-06-02 01:35:38 +0200

    week 4 assign

--- frontend_assignment/pages/index.tsx
+++ frontend_assignment/pages/index.tsx
@@ -6,8 +6,19 @@ import Head from "next/head"
 import React from "react"
 import styles from "../styles/Home.module.css"
 
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  example: string,
+  exampleRequired: string,
+};
+
 export default function Home() {
     const [logs, setLogs] = React.useState("Connect your wallet and greet!")
+	const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+	const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+
+	console.log(watch("example")) // watch input value by passing the name of it
 
     async function greet() {
         setLogs("Creating your Semaphore identity...")
@@ -68,15 +79,17 @@ export default function Home() {
             </Head>
 
             <main className={styles.main}>
-                <h1 className={styles.title}>Greetings</h1>
-
-                <p className={styles.description}>A simple Next.js/Hardhat privacy application with Semaphore.</p>
-
-                <div className={styles.logs}>{logs}</div>
-
-                <div onClick={() => greet()} className={styles.button}>
-                    Greet
-                </div>
+               <form onSubmit={handleSubmit(onSubmit)}>
+				  {/* register your input into the hook by invoking the "register" function */}
+				  <input defaultValue="test" {...register("example")} />
+				  
+				  {/* include validation with required or other standard HTML validation rules */}
+				  <input {...register("exampleRequired", { required: true })} />
+				  {/* errors will return when field validation fails  */}
+				  {errors.exampleRequired && <span>This field is required</span>}
+				  
+				  <input type="submit" />
+				</form>
             </main>
         </div>
     )
